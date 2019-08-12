f9350a1db2bd07f801a656f759655589f0c35ce6
bit <bit@DESKTOP-S8HQR48>
2019-08-12 13:11:34 +0900

    완료

--- src/prob01/Printer.java
+++ src/prob01/Printer.java
@@ -1,19 +1,29 @@
 package prob01;
  
 public class Printer{
-	public void println( int val ) {
-		System.out.println( val );
+	public void println(int data) {
+		System.out.println(data);
 	}
-
-	public void println( boolean val ) {
-		System.out.println( val );
+	public void println(double data) {
+		System.out.println(data);
+	}
+	public void println(boolean data) {
+		System.out.println(data);
+	}
+	public void println(String data) {
+		System.out.println(data);
+	}
+	public void println(int data1, boolean data2, double data3, String data4) {
+		println(data1);
+		println(data2);
+		println(data3);
+		println(data4);
 	}
 	
-	public void println( String val ) {
-		System.out.println( val );
+	public int sum(int data1, int data2, int data3) {
+		return sum(data1, data2, data3, 0, 0);
 	}
-	
-	public void println( double val ) {
-		System.out.println( val );
+	public int sum(int data1, int data2, int data3, int data4, int data5) {
+		return data1+data2+data3+data4+data5;
 	}
 }
